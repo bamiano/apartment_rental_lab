908b14e17983394dd1f87ed0d1d992cd7922ec1e
bamiano <bamiano@email.arizona.edu>
2014-10-11 15:59:18 -0700

    2nd Version

--- src/main.js
+++ src/main.js
@@ -3,14 +3,36 @@ var menu = require('node-menu');
 var app = require('./app.js');
 
 var building = new app.Building("Waterfront Tower");
+// var building refers to an instance of building named Waterfront Tower
 var people = [];
+
+
+people.push(new app.Person("Anna", "765-4321"));
+var john = new app.Manager("John", "700-4321");
+building.setManager(john);
+people.push(john);
+var devin = new app.Tenant("Devin", "765-1234");
+devin.addReference(new app.Person("Carl", "415 3536 222"));
+devin.addReference(new app.Person("Steve", "415 1111 222"));
+people.push(devin);
+people.push(new app.Tenant("Steve", "744-1234"));
+
+building.units.push(new app.Unit("12", building, 400, 2000));
+building.units.push(new app.Unit("13", building, 800, 3000));
+building.units.push(new app.Unit("14", building, 1800, 4500));
 
 // --------------------------------
 menu.addDelimiter('-', 40, building.address + " rental app");
 
-menu.addItem('Add manager', 
+menu.addItem('Add manager',
+ //Add manager is a string (first parameter)
+ // second parameter is a callback which takes in a function.
+ // third item is a null (third parameter)
+ // fourth item is an array of objects (fourth parameter)
+ // none of these items do anything until they are called in node with one of the number functions.
   function(name, contact) {
     var aManager = new app.Manager(name, contact);
+    // we create a new instance of a manager called app.manager
     aManager.addBuilding(building);
     building.setManager(aManager);
     people.push(new app.Manager(name, contact));
@@ -31,9 +53,12 @@ menu.addItem('Show tenants:',
   function() {
     for (var i = 0; i <= people.length; i++) {
       if (people[i] instanceof app.Tenant){
+        // we want to see if people(an object) is an instance of app.Tenant(a function) this tells us if people is a tenant
         console.log("\n" + people[i].name + " " + people[i].contact);
         var references = people[i].references;
+        // var references is equal to whatever person we're looking at
         if(!references) {continue;}
+        // above line is confusing and not explained
         for (var j = references.length - 1; j >= 0; j--) {
           console.log("-> Reference: " + references[j].name + " " + references[j].contact);
         };
@@ -67,12 +92,31 @@ menu.addItem('Show all units',
 menu.addItem('(implement me) Show available units', 
   function() {
       console.log("Implement me");
-    } 
+      var emptyUnits = building.availableUnits();
+     for (var i = 0; i < emptyUnits.length; i++){
+        console.log(" tenant: " + building.units[i].tenant +
+              " num: " + building.units[i].number + 
+                  " sqft: " + building.units[i].sqft +
+                  " rent: $" + building.units[i].rent);
+     }
+   }
 );
 
 menu.addItem('(implement me) Add tenant reference', 
   function(tenant_name, ref_name, ref_contact) {
       console.log("Implement me. Show error if tenant is unknown. Note: a reference is a person");
+      var reference = new app.Person(ref_name, ref_contact);
+
+      // If the tenant name entered in console is equal to the tenant.name that is inside the array and is also considered an instance of the tenant class, then we have the right person.
+      // We want to add references to the tenant.references array. In order to do so we need to check the people's array and see inside if there's a match of what we enter )tenant_name and what's inside tenant.name.
+      people.forEach(function(tenant){
+        // on the top of the page we created tenants that are being pushed into people array. if the person is a tenant, you can push references inside of him.
+        if (tenant.name === tenant_name && tenant instanceof app.Tenant){
+          tenant.references.push(reference);
+        }
+      });
+    
+
     },
     null, 
     [{'name': 'tenant_name', 'type': 'string'},
--- src/rental_property/building.js
+++ src/rental_property/building.js
@@ -23,10 +23,6 @@ Building.prototype.setManager = function(person) {
   }
 };
 
-   // it("should set @manager for a manager", function(){
-   //      myBuilding.setManager(manager);
-   //      expect(myBuilding.manager).to.eql(manager);
-   //    });
 
 Building.prototype.getManager = function(){
   // return this.manager 
@@ -39,8 +35,10 @@ Building.prototype.addTenant = function(unit, tenant) {
   // is a manager first and a tenant has 2 references
   // Note that tenenat does not belong to Building, but to Unit
   // ...
-  if (this.manager !== null && tenant.references.length >= 2) {
+  if (this.manager !== null && tenant.references.length >= 2 && 
+    unit.available() && this.units.indexOf(unit) !== -1) {
     unit.tenant = tenant;
+
   }
 
 };
@@ -48,16 +46,25 @@ Building.prototype.addTenant = function(unit, tenant) {
 Building.prototype.removeTenant = function(unit, tenant) {
   // remove tenant
   // ...
+  if (this.manager !== null && this.units.indexOf(unit) !== -1 && unit.tenant === tenant) {
+    unit.tenant = null;
+  }
 };
-
+// available units is a function, when called it returns this filter. in order to get everything out of the inner and outer function, we need two returns. the outer function returns the result of the inner function. the inner needs to return data to the outer. and the outer needs to return data to the entire function
+ //filter filters out only truthy things and places it in a new array 
 Building.prototype.availableUnits = function(){
   // return units available
   // ...
+  return this.units.filter(function (unit){
+    return unit.available();
+  });
 };
 
+
 Building.prototype.rentedUnits = function(){
-  // return rented units
-  // ...
+  return this.units.filter(function (unit){
+    return !unit.available();
+  });
 };
 
 module.exports = Building;
